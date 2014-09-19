bcbcbe3f30af9fd61b0a6c64e4aeb5841ab80e49
Andres-J. Cremades <andres.cremades@validad.net>
2014-09-19 11:46:37 +0200

    Update index.ejs & main.js

--- main.js
+++ main.js
@@ -13,11 +13,28 @@ var itemHtml = function (thumbnailSrc, title) {
     '</li>'
   ].join('');
 };
-$('#movieSearchForm').on('submit', function () {
-  var query = 'asdasd';
 
-  alert('submitting');
+
+$('#movieSearchForm').on('submit', function () {
+  var movieTitle = $('#movieSearchForm input').val();
+
+  $.getJSON('spiderman-api-call.json').success(function(response, movieTitle){
+    var movieListData =  response.movies;
+    var movieList = movieListData.map(function (movieItem) {
+
+    console.log('HOLA!');
+      if(movieItem.title === movieTitle)
+        return itemHtml(movieItem.posters.thumbnail, movieItem.title);
+    });
+
+    $('#resultList').get(0).innerHTML = [
+      '<ul>',
+        movieList.join(''),
+      '</ul>'
+    ].join('');
+  });
 });
+
 var myApp = function (response) {
   var movieListData = response.movies;
 
@@ -26,10 +43,12 @@ var myApp = function (response) {
   });
 
   $('#movieList').get(0).innerHTML = [
-      '<ul>',
+    '<ul>',
         movieList.join(''),
       '</ul>'
   ].join('');
+
+  console.log('HALLO!');
 };
 
 
